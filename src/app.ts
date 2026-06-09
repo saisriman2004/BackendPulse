e640a8b323bfb81fd4e7b3eac580000a3d02b364
saisriman2004 <saisriman034@gmail.com>
2026-06-09 10:42:21 -0700

    Set up Express TypeScript Backend

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/",(req,res)=>{
+    res.status(200).json({
+        message: "Welcome to the application"
+    })
+})
+
+app.get("/health",(req,res)=>{
+    res.status(200).json({
+        message:"Backend Server is running...."
+    })
+})
+
+export default app;
